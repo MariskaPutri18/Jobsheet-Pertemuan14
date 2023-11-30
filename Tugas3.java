73e8e5163b73113e8b615b87666fa5496f8771f5
MariskaPutri18 <mariskaputri960@gmail.com>
2023-11-30 09:41:09 +0700

    commit tugas 4

--- Tugas3.java
+++ Tugas3.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Tugas3 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan n: ");
+        int n = sc.nextInt();
+
+        if (cekPrima(n, 2)) {
+            System.out.println(n + " adalah bilangan prima.");
+        } else {
+            System.out.println(n + " bukan bilangan prima.");
+        }
+    }
+
+    static boolean cekPrima(int n, int i) {
+        if (n <= 1) {
+            return false; 
+        } else if (i < n) {
+            if (n % i == 0) {
+                return false; 
+            } else {
+                return cekPrima(n, i + 1); 
+            }
+        } else {
+            return true; 
+    }
 }
+}
+
+
