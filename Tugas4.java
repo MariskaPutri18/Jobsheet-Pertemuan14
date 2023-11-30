35afaf8d22807f9ef454088d8b528fe1788c4dda
MariskaPutri18 <mariskaputri960@gmail.com>
2023-11-30 09:51:25 +0700

    commit tugas 4

--- Tugas4.java
+++ Tugas4.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
 public class Tugas4 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan bulan ke: ");
+        int bulanKe = sc.nextInt();
+        int jumlahPasangan = hitungPasanganMarmut(bulanKe);
+        System.out.println("Jumlah pasangan marmut pada bulan ke-" + bulanKe + " adalah: " + jumlahPasangan);
+    }
+
+    static int hitungPasanganMarmut(int bulan) {
+        if (bulan <= 2) {
+            return 1; 
+        } else {
+            return hitungPasanganMarmut(bulan - 1) + hitungPasanganMarmut(bulan - 2);
+        }
+    }
+
+
 }
